76cba6653e7d0ab2ca9eee83c3f194585a241544
Wendy0204 <wendyizquierdooliveros@gmail.com>
2020-04-03 23:58:33 -0500

    Second

--- src/app/pages/camp/camp.page.ts
+++ src/app/pages/camp/camp.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import 'firebase/database';
+
 @Component({
   selector: 'app-camp',
   templateUrl: './camp.page.html',
   styleUrls: ['./camp.page.scss'],
 })
-export class CampPage implements OnInit {
+export class CampPage {
 
-  constructor() { }
+  nombre: string;
+  descripcion: string;
+  direccion: string;
+  fecha: string;
+  todo: any;
+  item: Observable<any>;
+  itemRef: AngularFireObject<any>;
+  constructor(db: AngularFireDatabase)
 
-  ngOnInit() {
-  }
-
+  constructor(db: AngularFireDatabase) {
+    this.itemRef = db.object('brigadas');
+    this.item = this.itemRef.valueChanges();
+    this.itemRef.snapshotChanges().subscribe(action => {
+    console.log(action.type);
+    console.log(action.key);
+    console.log(action.payload.val());
+    this.todo = action.payload.val();
+    this.nombre = this.todo.nombre;
+    this.descripcion = this.todo.descripcion;
+    this.direccion = this.todo.direccion;
+    this.fecha = this.todo;
+    });
+   }
 }
--- src/app/pages/card/card.page.ts
+++ src/app/pages/card/card.page.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import 'firebase/database';
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.page.html',
   styleUrls: ['./card.page.scss'],
 })
-export class CardPage implements OnInit {
+export class CardPage{
 
-  constructor() { }
+  nombre: string;
+  descripcion: string;
+  direccion: string;
+  fecha: string;
+  todo: any;
+  item: Observable<any>;
+  itemRef: AngularFireObject<any>;
+  constructor(db: AngularFireDatabase)
 
-  ngOnInit() {
+  constructor(db: AngularFireDatabase) { 
+    this.itemRef = db.object('BRIGADAS');
+    this.item = this.itemRef.valueChanges();
+    this.itemRef.snapshotChanges().subscribe(action => {
+    console.log(action.type);
+    console.log(action.key);
+    console.log(action.payload.val());
+    this.todo = action.payload.val();
+    this.nombre = this.todo.nombre;
+    this.descripcion = this.todo.descricion;
+    this.direccion = this.todo.direccion;
+
+    this.nombre = this.todo.nombre2;
+    this.descripcion = this.todo.descricion2;
+    this.direccion = this.todo.direccion2;
+
+    this.nombre = this.todo.nombre3;
+    this.descripcion = this.todo.descricion3;
+    this.direccion = this.todo.direccion3;
+    });
   }
+
 
 }
